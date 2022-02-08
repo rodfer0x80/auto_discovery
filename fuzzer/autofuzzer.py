96d3782e7e137776b1dee572def4f1d1fa9e4aec
trevalkov <rodrigolf080@gmail.com>
2022-02-08 13:12:06 +0000

    yolo

---
+++ fuzzer/autofuzzer.py
@@ -0,0 +1,48 @@
+import socket
+
+
+class Autofuzzer:
+    def __init__(self, server: str, port: int):
+        self.server = server
+        self.port = port
+        self.conn = self.connect(self.server, self.port)
+
+
+    def connect(self):
+        try:
+            self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except:
+            sys.stderr.write("[!] Error creating socket object\n")
+        try:
+            self.conn.connect((server, port))
+        except:
+            sys.stderr.write("[!] Error estabilishing socket conection\n")
+
+
+    def refresh_connection(self):
+        try:
+            self.conn.close()
+        except:
+            sys.stderr.write("[!] Error closing socket connection\n")
+        self.connect()
+
+
+    def overflow_input(num_chars=128):
+        for i in range(1, num_chars):
+        try:
+            self.read_until()
+            data = 'A' * i + '\n'
+            data = bytes(data, encoding='utf-8')
+            self.conn.send(data)
+        except:
+            print(f"[*] Server crashed with input size {i}")
+        finally:
+            self.conn.refresh()
+
+
+    def read_until(self, delim=b':'):    
+        buf = b''
+        while not buf.endswith(delim):
+            buf += self.conn.recv(1)
+        return buf
+
